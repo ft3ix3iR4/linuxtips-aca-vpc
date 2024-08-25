431d26ec5c12db9533867a98cb1ed4504983b8dd
fteixeira <51334332+ft3ix3iR4@users.noreply.github.com>
2024-08-25 09:03:14 -0300

    feat setup nat gateway

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -4,3 +4,12 @@ resource "aws_eip" "vpc_eip_la" {
     Name = format("%s-eip-1a", var.project_name)
   }
 }
+
+resource "aws_nat_gateway" "nat_1a" {
+  allocation_id = aws_eip.vpc_eip_la.id
+  subnet_id     = aws_subnet.public_subnet_1a
+
+  tags = {
+    Name = format("%s-nat-1a", var.project_name)
+  }
+}
