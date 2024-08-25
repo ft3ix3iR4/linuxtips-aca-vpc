2b7dfb7e5b12f38645d947522932819363af5c9a
fteixeira <51334332+ft3ix3iR4@users.noreply.github.com>
2024-08-25 11:03:21 -0300

    feat route table for private subnets

--- private_subnets.tf
+++ private_subnets.tf
@@ -69,3 +69,18 @@ resource "aws_route" "private_access_1c" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_nat_gateway.nat_1c.id
 }
+
+resource "aws_route_table_association" "private_1a" {
+  subnet_id      = aws_subnet.private_subnet_1a.id
+  route_table_id = aws_route_table.private_internet_access.id
+}
+
+resource "aws_route_table_association" "private_1b" {
+  subnet_id      = aws_subnet.private_subnet_1b.id
+  route_table_id = aws_route_table.private_internet_access.id
+}
+
+resource "aws_route_table_association" "private_1c" {
+  subnet_id      = aws_subnet.private_subnet_1c.id
+  route_table_id = aws_route_table.private_internet_access.id
+}
