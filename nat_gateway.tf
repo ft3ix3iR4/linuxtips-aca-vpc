01ad3d1d2c9c56bbfb36f48ff83093c585f27bb0
fteixeira <51334332+ft3ix3iR4@users.noreply.github.com>
2024-08-25 09:11:10 -0300

    fix nat gateway

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -7,7 +7,7 @@ resource "aws_eip" "vpc_eip_la" {
 
 resource "aws_nat_gateway" "nat_1a" {
   allocation_id = aws_eip.vpc_eip_la.id
-  subnet_id     = aws_subnet.public_subnet_1a
+  subnet_id     = aws_subnet.public_subnet_1a.id
 
   tags = {
     Name = format("%s-nat-1a", var.project_name)
